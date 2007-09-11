71429f5fe9fb29b9b47940f8ddbed000588b0150
derjoerg <derjoerg@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-09-11 06:43:46 +0000

    included BE-function and BLANK_IMG_URL setting
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extjs/branches/1.x@6475 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_extjs.php
+++ class.tx_extjs.php
@@ -41,11 +41,73 @@ class tx_extjs {
 	}
 
 	/**
-	 * include the library and other data for page rendering
+	 * include the library and other data for page renderung into a BE-site
+	 * any configuration has to be done before with the set-fucntions
+	 */
+	function BEincludeLib() {
+		global $BACK_PATH;
+		$returnArr = array(
+			'css' => '',
+			'additional_js' => '',
+			'js' => '',
+		);
+		$fileArr = tx_extjs::getLibs($BACK_PATH . '../' . t3lib_extMgm::siteRelPath('extjs'));
+
+		if (!$GLOBALS['tx_extjs']['tx_extjs_inc']) {
+			$returnArr['css'] = implode("\n", $fileArr['css']);
+			$returnArr['additional_js'] = $fileArr['additional_js'];
+			$returnArr['js'] = implode("\n", $fileArr['js']);
+			$GLOBALS['tx_extjs']['tx_extjs_inc'] = TRUE;
+		}
+
+		return $returnArr;
+	}
+
+	/**
+	 * include the library and other data for page rendering into a FE-site
 	 * any configuration has to be done before with the set-functions
 	 */
 	function includeLib()	{
-		// first we look, wich kind of plugins should be loaded (compressed or uncompressed)
+		$fileArr = tx_extjs::getLibs(t3lib_extMgm::siteRelPath('extjs'));
+
+		if (!$GLOBALS['tx_extjs']['tx_extjs_inc']) {
+
+			// First the css
+			if (isset($fileArr['css'])) {
+
+				foreach ($fileArr['css'] as $key => $script) {
+					$GLOBALS['TSFE']->additionalCSS['tx_extjs_' . $key] = $script;
+				}
+
+			}
+
+			// The blank_img-setting
+			if (isset($fileArr['additional_js'])) {
+				$GLOBALS['TSFE']->additionalJavaScript['tx_extjs_blankimg'] = $fileArr['additional_js'];
+			}
+
+			// Now the js
+			if (isset($fileArr['js'])) {
+
+				foreach ($fileArr['js'] as $key => $script) {
+					$GLOBALS['TSFE']->additionalHeaderData['tx_extjs_script_' . $key . '_inc'] = $script;
+				}
+
+			}
+
+			$GLOBALS['tx_extjs']['tx_extjs_inc'] = TRUE;
+		}
+
+	}
+
+	function getLibs($path) {
+		$returnArr = array(
+			'css' => array(),
+			'additional_js' => '',
+			'js' => array(),
+		);
+
+		// first we look, which kind of plugins should be loaded (compressed or uncompressed)
 		if (!isset($GLOBALS['tx_extjs']['compressed']))	{
 
 			if (isset($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_extjs.']['compressed']))	{
@@ -61,14 +123,7 @@ class tx_extjs {
 			}
 
 		}
-/**
-		// add extjs to page content
-		if (!$GLOBALS['tx_extjs']['tx_extjs_script_inc']) {
-			// add extjs to page header
-			$GLOBALS['TSFE']->additionalHeaderData['tx_extjs_script_inc'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('extjs') . ($GLOBALS['tx_extjs']['compressed']==false?'uncompressed_':'') . 'src/ext-all.js"></script>';
-			$GLOBALS['tx_extjs']['tx_extjs_script_inc'] = TRUE;
-		}
-**/
+
 		// the config is parsed the following way
 		// 1. has the user set something within an application
 		// 2. is something defined by TS
@@ -94,66 +149,67 @@ class tx_extjs {
 
 		}
 
-		// add everything to page content
-		if (!$GLOBALS['tx_extjs']['tx_extjs_script_inc']) {
-			$GLOBALS['TSFE']->additionalCSS['tx_extjs_base'] = '@import url("' . t3lib_extMgm::siteRelPath('extjs') . 'resources/css/ext-all.css");';
+		// add everything to return-array
+		$returnArr['css'][] = '@import url("' . $path . 'resources/css/ext-all.css");';
 
-			// first the css
-			switch ($GLOBALS['tx_extjs']['resource'])	{
-				case 'aero':
-					$GLOBALS['TSFE']->additionalCSS['tx_extjs_additional'] = '@import url("' . t3lib_extMgm::siteRelPath('extjs') . 'resources/css/ytheme-aero.css");';
-					break;
-				case 'default':
-					break;
-				case 'gray':
-					$GLOBALS['TSFE']->additionalCSS['tx_extjs_additional'] = '@import url("' . t3lib_extMgm::siteRelPath('extjs') . 'resources/css/ytheme-gray.css");';
-					break;
-				case 'vista':
-					$GLOBALS['TSFE']->additionalCSS['tx_extjs_additional'] = '@import url("' . t3lib_extMgm::siteRelPath('extjs') . 'resources/css/ytheme-vista.css");';
-					break;
-			}
-
-			// second the adapter
-			// NOTE: the base-adapter-lib has to be included externally before this
-			switch ($GLOBALS['tx_extjs']['adapter'])	{
-				case 'jquery':
-					$GLOBALS['TSFE']->additionalHeaderData['tx_extjs_script_adapter1'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('extjs') . 'adapter/jquery/jquery-plugins.js"></script>';
-					$GLOBALS['TSFE']->additionalHeaderData['tx_extjs_script_adapter2'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('extjs') . 'adapter/jquery/ext-jquery-adapter.js"></script>';
-					break;
-				case 'prototype':
-					$GLOBALS['TSFE']->additionalHeaderData['tx_extjs_script_adapter1'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('extjs') . 'adapter/prototype/ext-prototype-adapter.js"></script>';
-					break;
-				case 'yui':
-					$GLOBALS['TSFE']->additionalHeaderData['tx_extjs_script_adapter1'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('extjs') . 'adapter/yui/yui-utilities.js"></script>';
-					$GLOBALS['TSFE']->additionalHeaderData['tx_extjs_script_adapter2'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('extjs') . 'adapter/yui/ext-yui-adapter.js"></script>';
-					break;
-			}
-
-			$GLOBALS['TSFE']->additionalHeaderData['tx_extjs_script_inc'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('extjs') . ($GLOBALS['tx_extjs']['compressed']==false?'uncompressed_':'') . 'src/ext-all.js"></script>';
-			$GLOBALS['tx_extjs']['tx_extjs_script_inc'] = TRUE;
+		// first the css
+		switch ($GLOBALS['tx_extjs']['resource'])	{
+			case 'aero':
+				$returnArr['css'][] = '@import url("' . $path . 'resources/css/ytheme-aero.css");';
+				$returnArr['additional_js'] = 'Ext.BLANK_IMAGE_URL = "' . $path . 'resources/images/aero/s.gif";';
+				break;
+			case 'default':
+				$returnArr['additional_js'] = 'Ext.BLANK_IMAGE_URL = "' . $path . 'resources/images/default/s.gif";';
+				break;
+			case 'gray':
+				$returnArr['css'][] = '@import url("' . $path . 'resources/css/ytheme-gray.css");';
+				$returnArr['additional_js'] = 'Ext.BLANK_IMAGE_URL = "' . $path . 'resources/images/gray/s.gif";';
+				break;
+			case 'vista':
+				$returnArr['css'][] = '@import url("' . $path . 'resources/css/ytheme-vista.css");';
+				$returnArr['additional_js'] = 'Ext.BLANK_IMAGE_URL = "' . $path . 'resources/images/vista/s.gif";';
+				break;
 		}
 
+		// second the adapter
+		// NOTE: the base-adapter-lib has to be included externally before this
+		switch ($GLOBALS['tx_extjs']['adapter'])	{
+			case 'jquery':
+				$returnArr['js'][] = '<script type="text/javascript" src="' . $path . 'adapter/jquery/jquery-plugins.js"></script>';
+				$returnArr['js'][] = '<script type="text/javascript" src="' . $path . 'adapter/jquery/ext-jquery-adapter.js"></script>';
+				break;
+			case 'prototype':
+				$returnArr['js'][] = '<script type="text/javascript" src="' . $path . 'adapter/prototype/ext-prototype-adapter.js"></script>';
+				break;
+			case 'yui':
+				$returnArr['js'][] = '<script type="text/javascript" src="' . $path . 'adapter/yui/yui-utilities.js"></script>';
+				$returnArr['js'][] = '<script type="text/javascript" src="' . $path . 'adapter/yui/ext-yui-adapter.js"></script>';
+				break;
+		}
+
+		$returnArr['js'][] = '<script type="text/javascript" src="' . $path . ($GLOBALS['tx_extjs']['compressed']==false?'uncompressed_':'') . 'src/ext-all.js"></script>';
+		return $returnArr;
 	}
 
 	/**
 	 * set value which adapter should be used (jquery, prototype, yui)
 	 */
-	function setAdapter($sAdapter='yui')	{
-		$GLOBALS['tx_extjs']['adapter'] = (string)$sAdapter;
+	function setAdapter($adapter='yui')	{
+		$GLOBALS['tx_extjs']['adapter'] = (string)$adapter;
 	}
 
 	/**
 	 * set value which theme should be used (aero, default, gray, vista)
 	 */
-	function setResource($sResource='default')	{
-		$GLOBALS['tx_extjs']['resource'] = (string)$sResource;
+	function setResource($resource='default')	{
+		$GLOBALS['tx_extjs']['resource'] = (string)$resource;
 	}
 
 	/**
 	 * set value if compressed scripts should be included
 	 */
-	function setCompressed($bVar=TRUE)	{
-		$GLOBALS['tx_extjs']['compressed'] = (bool)$bVar;
+	function setCompressed($var=TRUE)	{
+		$GLOBALS['tx_extjs']['compressed'] = (bool)$var;
 	}
 
 }
